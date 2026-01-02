9a4c3f069086d9d533a48be1b6a9978fd7d52af0
ren0o9 <22f23637@mec.edu.om>
2026-01-02 23:23:12 +0400

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,12 @@
 Initialize maxSales = 0
 Initialize bestProduct = null
+Initialize productCount = 0
 
 For each product in products:
+    productCount = productCount + 1
     If totalSales > maxSales:
         maxSales = totalSales
         bestProduct = product
 
 Output bestProduct
+Output productCount

720f71f9cd704b25f00cc5bbe675f863a411f0d6
ren0o9 <22f23637@mec.edu.om>
2026-01-02 22:43:41 +0400

    Add initial best-selling product algorithm

---
+++ algorithm.py
@@ -0,0 +1,9 @@
+Initialize maxSales = 0
+Initialize bestProduct = null
+
+For each product in products:
+    If totalSales > maxSales:
+        maxSales = totalSales
+        bestProduct = product
+
+Output bestProduct
